ceffcd7cb0bb43d7f3c1ee9a00aa956b2a5a4baa
pdruart <pdruart@student.codam.nl>
2021-05-22 16:57:50 +0200

    progress

--- push_swap.c
+++ push_swap.c
@@ -1,6 +1,48 @@
 #include "push_swap.h"
+#include "libft/libft.h"
+#include "stdlib.h"
+
+void	call_error(void)
+{
+	exit(1);
+}
+
+int	is_number(char *str)
+{
+	if (str == NULL || (*str != '-' && !ft_isdigit(*str)))
+		return (0);
+	str++;
+	while (ft_isdigit(*str))
+		str++;
+	return (*str == '\0');
+}
+
+t_list	*parse_args(int argc, char *argv[])
+{
+	t_list	*first;
+	t_list	*relative;
+	int		i;
+	int		numb;
+
+	i = 0;
+	numb = 0;
+	if (argv[i])
+	first = ft_lstnew(NULL);//eaugh
+	if (first == NULL)
+		return (NULL);
+	relative = first;
+	while (i < argc)
+	{
+
+	}
+	return (first);
+}
 
 int	main(int argc, char *argv[])
 {
-	;
+	t_list	*numbs;
+
+	numbs = parse_args(argc, argv);
+	if (numbs == NULL)
+		call_error();
 }
